9fad0deaa0bebd2d3800b6dc52c37ad0b31c4c66
RaZoR-sHaRp05 <myseangalloway@gmail.com>
2026-02-18 13:02:00 -0500

    Added planet textures

--- Project1_MGalloway18/SpaceJam.py
+++ Project1_MGalloway18/SpaceJam.py
@@ -43,10 +43,23 @@ class Game(ShowBase):
         self.Planet6.setPos(-560, -54, -267)
         self.Planet6.setScale(85)
 
+        texPlan1 = self.loader.loadTexture("Assets/Textures/Volcanic.png")
+        self.Planet1.setTexture(texPlan1, 1)
+        texPlan2 = self.loader.loadTexture("Assets/Textures/Tropical.png")
+        self.Planet2.setTexture(texPlan2, 1)
+        texPlan3 = self.loader.loadTexture("Assets/Textures/Gaseous1.png")
+        self.Planet3.setTexture(texPlan3, 1)
+        texPlan4 = self.loader.loadTexture("Assets/Textures/Martian.png")
+        self.Planet4.setTexture(texPlan4, 1)
+        texPlan5 = self.loader.loadTexture("Assets/Textures/Venusian.png")
+        self.Planet5.setTexture(texPlan5, 1)
+        texPlan6 = self.loader.loadTexture("Assets/Textures/Icy.png")
+        self.Planet6.setTexture(texPlan6, 1)
+
         self.SpaceStation = self.loader.loadModel("Assets/SpaceStation1B/spaceStation.x")
         self.SpaceStation.reparentTo(self.render)
 
-        self.SpaceShip = self.loader.loadModel("Assets/Phaser/phaser.egg")
+        self.SpaceShip = self.loader.loadModel("Assets/Phaser/phaser.x")
         self.SpaceShip.reparentTo(self.render)
         self.SpaceShip.setPos(0, 0, 50)
     
