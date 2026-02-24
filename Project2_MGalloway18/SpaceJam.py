b74a481955304a7aa5d3f77005fa529599de3b2e
RaZoR-sHaRp05 <myseangalloway@gmail.com>
2026-02-24 13:44:21 -0500

    Added SpaceStation and SpaceShip classes

--- Project2_MGalloway18/SpaceJam.py
+++ Project2_MGalloway18/SpaceJam.py
@@ -11,18 +11,15 @@ class Game(ShowBase):
         self.Universe = SpaceJamClasses.Universe(self.loader, "Assets/Universe/Universe.x", self.render, "Universe", "Assets/Textures/Sea-of-Stars.jpg", 15000)
 
         self.Planet1 = SpaceJamClasses.Planet(self.loader, "Assets/protoPlanet.x", self.render, "Planet1", "Assets/Textures/Volcanic.png", (180, 4580, 70), 500)
-        self.Planet2 = SpaceJamClasses.Planet(self.loader, "Assets/protoPlanet.x", self.render, "Planet1", "Assets/Textures/Tropical.png", (6000, 8000, 20), 800)
-        self.Planet3 = SpaceJamClasses.Planet(self.loader, "Assets/protoPlanet.x", self.render, "Planet1", "Assets/Textures/Gaseous1.png", (1000, 50, 900), 100)
-        self.Planet4 = SpaceJamClasses.Planet(self.loader, "Assets/protoPlanet.x", self.render, "Planet1", "Assets/Textures/Martian.png", (400, 456, -2000), 50)
-        self.Planet5 = SpaceJamClasses.Planet(self.loader, "Assets/protoPlanet.x", self.render, "Planet1", "Assets/Textures/Venusian.png", (-10000, 36, 756), 78)
-        self.Planet6 = SpaceJamClasses.Planet(self.loader, "Assets/protoPlanet.x", self.render, "Planet1", "Assets/Textures/Icy.png", (-560, -54, -267), 85)
+        self.Planet2 = SpaceJamClasses.Planet(self.loader, "Assets/protoPlanet.x", self.render, "Planet2", "Assets/Textures/Tropical.png", (6000, 8000, 20), 800)
+        self.Planet3 = SpaceJamClasses.Planet(self.loader, "Assets/protoPlanet.x", self.render, "Planet3", "Assets/Textures/Gaseous1.png", (1000, 50, 900), 100)
+        self.Planet4 = SpaceJamClasses.Planet(self.loader, "Assets/protoPlanet.x", self.render, "Planet4", "Assets/Textures/Martian.png", (400, 456, -2000), 50)
+        self.Planet5 = SpaceJamClasses.Planet(self.loader, "Assets/protoPlanet.x", self.render, "Planet5", "Assets/Textures/Venusian.png", (-10000, 36, 756), 78)
+        self.Planet6 = SpaceJamClasses.Planet(self.loader, "Assets/protoPlanet.x", self.render, "Planet6", "Assets/Textures/Icy.png", (-560, -54, -267), 85)
 
-        self.SpaceStation = self.loader.loadModel("Assets/SpaceStation1B/spaceStation.x")
-        self.SpaceStation.reparentTo(self.render)
+        self.SpaceStation = SpaceJamClasses.SpaceStation(self.loader, "Assets/SpaceStation1B/spaceStation.x", self.render, "SpaceStation", (0, 0, 0), 1)
 
-        self.SpaceShip = self.loader.loadModel("Assets/Phaser/phaser.x")
-        self.SpaceShip.reparentTo(self.render)
-        self.SpaceShip.setPos(0, 0, 50)
+        self.SpaceShip = SpaceJamClasses.SpaceShip(self.loader, "Assets/Phaser/phaser.x", self.render, "SpaceShip", (0, 0, 50), 1)
 
 game = Game()
 game.SetupScene()
--- Project2_MGalloway18/SpaceJamClasses.py
+++ Project2_MGalloway18/SpaceJamClasses.py
@@ -21,3 +21,21 @@ class Planet(ShowBase):
         self.modelNode.setName(nodeName)
         tex = loader.loadTexture(texPath)
         self.modelNode.setTexture(tex, 1)
+
+class SpaceStation(ShowBase):
+    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, posVec: Vec3, scaleVec: float):
+        self.modelNode = loader.loadModel(modelPath)
+        self.modelNode.reparentTo(parentNode)
+        self.modelNode.setPos(posVec)
+        self.modelNode.setScale(scaleVec)
+
+        self.modelNode.setName(nodeName)
+
+class SpaceShip(ShowBase):
+    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, posVec: Vec3, scaleVec: float):
+        self.modelNode = loader.loadModel(modelPath)
+        self.modelNode.reparentTo(parentNode)
+        self.modelNode.setPos(posVec)
+        self.modelNode.setScale(scaleVec)
+
+        self.modelNode.setName(nodeName)
