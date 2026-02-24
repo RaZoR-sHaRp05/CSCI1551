134638e1c56535dc6d5c0f0c8b0eb3abf13f81cf
RaZoR-sHaRp05 <myseangalloway@gmail.com>
2026-02-24 13:13:21 -0500

    Added planet class

---
+++ Project2_MGalloway18/SpaceJamClasses.py
@@ -0,0 +1,13 @@
+from direct.showbase.ShowBase import ShowBase
+from panda3d.core import *
+
+class Planet(ShowBase):
+    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, texPath: str, posVec: Vec3, scaleVec: float):
+        self.modelNode = loader.loadModel(modelPath)
+        self.modelNode.reparentTo(parentNode)
+        self.modelNode.setPos(posVec)
+        self.modelNode.setScale(scaleVec)
+
+        self.modelNode.setName(nodeName)
+        tex = loader.loadTexture(texPath)
+        self.modelNode.setTexture(tex, 1)
