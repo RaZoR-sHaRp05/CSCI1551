b49ada958b7bc736c75ad794e53efab3daa1574f
RaZoR-sHaRp05 <myseangalloway@gmail.com>
2026-03-29 11:42:01 -0400

    Created folder

---
+++ Project6_MGalloway18/SpaceJam.py
@@ -0,0 +1,93 @@
+from direct.showbase.ShowBase import ShowBase
+from panda3d.core import Filename
+from panda3d.core import CollisionTraverser, CollisionHandlerPusher
+from panda3d.core import *
+import SpaceJamClasses
+import DefensePaths
+from direct.gui.OnscreenImage import OnscreenImage
+
+class Game(ShowBase):
+
+    def __init__(self):
+        ShowBase.__init__(self)
+        fullCyle = 100
+        angle = 0
+
+        self.SetupScene()
+        self.SetCamera()
+        self.EnableHUD()
+
+        for j in range(fullCyle):
+            SpaceJamClasses.Drone.droneCount += 1
+            nickName = "Drone" + str(SpaceJamClasses.Drone.droneCount)
+
+            self.DrawCircleXDefense(self.Planet2, nickName, angle)
+            self.DrawCircleYDefense(self.Planet4, nickName, angle)
+            self.DrawCircleZDefense(self.Planet5, nickName, angle)
+            angle = angle + 0.06
+            self.DrawCloudDefense(self.Planet1, nickName)
+            self.DrawBaseballDefense(self.SpaceStation, nickName, j, fullCyle, 2)
+        
+        self.cTrav = CollisionTraverser()
+        self.cTrav.traverse(self.render)
+        self.pusher = CollisionHandlerPusher()
+        self.pusher.addCollider(self.Spaceship.collisionNode, self.Spaceship.modelNode)
+        self.cTrav.addCollider(self.Spaceship.collisionNode, self.pusher)
+        self.cTrav.showCollisions(self.render)
+
+    def SetupScene(self):
+        self.Universe = SpaceJamClasses.Universe(self.loader, "Assets/Universe/Universe.x", self.render, "Universe", "Assets/Textures/Sea-of-Stars.jpg", (0, 0, 0), 15000)
+
+        self.Planet1 = SpaceJamClasses.Planet(self.loader, "Assets/protoPlanet.x", self.render, "Planet1", "Assets/Textures/Volcanic.png", (180, 4580, 70), 500)
+        self.Planet2 = SpaceJamClasses.Planet(self.loader, "Assets/protoPlanet.x", self.render, "Planet2", "Assets/Textures/Tropical.png", (6000, 8000, 20), 800)
+        self.Planet3 = SpaceJamClasses.Planet(self.loader, "Assets/protoPlanet.x", self.render, "Planet3", "Assets/Textures/Gaseous1.png", (1000, 50, 900), 100)
+        self.Planet4 = SpaceJamClasses.Planet(self.loader, "Assets/protoPlanet.x", self.render, "Planet4", "Assets/Textures/Martian.png", (400, 456, -2000), 50)
+        self.Planet5 = SpaceJamClasses.Planet(self.loader, "Assets/protoPlanet.x", self.render, "Planet5", "Assets/Textures/Venusian.png", (-10000, 36, 756), 78)
+        self.Planet6 = SpaceJamClasses.Planet(self.loader, "Assets/protoPlanet.x", self.render, "Planet6", "Assets/Textures/Icy.png", (-560, -54, -267), 85)
+
+        self.SpaceStation = SpaceJamClasses.SpaceStation(self.loader, "Assets/SpaceStation1B/spaceStation.egg", self.render, "SpaceStation", (0, 0, 0), 1)
+
+        self.Spaceship = SpaceJamClasses.Spaceship(self.loader, self.accept, "Assets/Dumbledore/Dumbledore.egg", self.render, "Spaceship", (0, 0, 50), 1)
+        self.Spaceship.SetKeyBindings()
+
+    def SetCamera(self):
+        self.disableMouse()
+        self.camera.reparentTo(self.Spaceship.modelNode)
+        self.camera.setFluidPos(0, -20, 0)
+
+    def EnableHUD(self):
+        self.Hud = OnscreenImage(image = "Assets/Hud/Reticle3b.png", pos = Vec3(0.065, 0, 0.05), scale = 0.1)
+        self.Hud.setTransparency(TransparencyAttrib.MAlpha)
+
+    def DrawCircleXDefense(self, targetObject, droneName, angle):
+        unitVec = DefensePaths.CircleX(angle)
+        unitVec.normalize()
+        position = unitVec * (targetObject.modelNode.getSx() * 2 + 200) + targetObject.modelNode.getPos()
+        SpaceJamClasses.Drone(self.loader, "Assets/DroneDefender/DroneDefender.obj", self.render, droneName, position, 2)
+
+    def DrawCircleYDefense(self, targetObject, droneName, angle):
+        unitVec = DefensePaths.CircleY(angle)
+        unitVec.normalize()
+        position = unitVec * (targetObject.modelNode.getSx() * 2 + 200) + targetObject.modelNode.getPos()
+        SpaceJamClasses.Drone(self.loader, "Assets/DroneDefender/DroneDefender.obj", self.render, droneName, position, 2)
+    
+    def DrawCircleZDefense(self, targetObject, droneName, angle):
+        unitVec = DefensePaths.CircleZ(angle)
+        unitVec.normalize()
+        position = unitVec * (targetObject.modelNode.getSx() * 2 + 200) + targetObject.modelNode.getPos()
+        SpaceJamClasses.Drone(self.loader, "Assets/DroneDefender/DroneDefender.obj", self.render, droneName, position, 2)
+
+    def DrawCloudDefense(self, targetObject, droneName):
+        unitVec = DefensePaths.Cloud()
+        unitVec.normalize()
+        position = unitVec * (targetObject.modelNode.getSx() * 2 + 200) + targetObject.modelNode.getPos()
+        SpaceJamClasses.Drone(self.loader, "Assets/DroneDefender/DroneDefender.obj", self.render, droneName, position, 2)
+
+    def DrawBaseballDefense(self, targetObject, droneName, step, numSeams, radius = 1):
+        unitVec = DefensePaths.BaseballSeams(step, numSeams, B = 0.4)
+        unitVec.normalize()
+        position = unitVec * radius * (targetObject.modelNode.getSx() * 1.3 + 150) + targetObject.modelNode.getPos()
+        SpaceJamClasses.Drone(self.loader, "Assets/DroneDefender/DroneDefender.obj", self.render, droneName, position, 2)
+
+game = Game()
+game.run()
---
+++ Project6_MGalloway18/SpaceJamClasses.py
@@ -0,0 +1,230 @@
+from direct.showbase.ShowBase import ShowBase
+from panda3d.core import *
+from direct.task import Task
+from CollideObjectBase import *
+from typing import Callable
+
+class Universe(InverseSphereCollideObject):
+    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, texPath: str, posVec: Vec3, scaleVec: float):
+        super(Universe, self).__init__(loader, modelPath, parentNode, nodeName, Vec3(0, 0, 0), 0.9)
+        self.modelNode.setPos(posVec)
+        self.modelNode.setScale(scaleVec)
+        self.modelNode.setName(nodeName)
+        tex = loader.loadTexture(texPath)
+        self.modelNode.setTexture(tex, 1)
+
+class Planet(SphereCollidableObject):
+    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, texPath: str, posVec: Vec3, scaleVec: float):
+        super(Planet, self).__init__(loader, modelPath, parentNode, nodeName, Vec3(0, 0, 0), 1.1)
+        self.modelNode.setPos(posVec)
+        self.modelNode.setScale(scaleVec)
+
+        self.modelNode.setName(nodeName)
+        tex = loader.loadTexture(texPath)
+        self.modelNode.setTexture(tex, 1)
+
+class SpaceStation(CapsuleCollideObject):
+    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, posVec: Vec3, scaleVec: float):
+        super(SpaceStation, self).__init__(loader, modelPath, parentNode, nodeName, 1, -1, 5, 1, -1, -5, 10)
+        self.modelNode.setPos(posVec)
+        self.modelNode.setScale(scaleVec)
+
+        self.modelNode.setName(nodeName)
+
+class Spaceship(SphereCollidableObject):
+    def __init__(self, loader: Loader, accept: Callable[[str, Callable], None], modelPath: str, parentNode: NodePath, nodeName: str, posVec: Vec3, scaleVec: float):
+        super(Spaceship, self).__init__(loader, modelPath, parentNode, nodeName, Vec3(0.35, 0, 0.2), 1.5)
+        self.accept = accept
+        self.modelNode.setPos(posVec)
+        self.modelNode.setScale(scaleVec)
+
+        self.modelNode.setName(nodeName)
+
+        self.reloadTime = 0.25
+        self.missileDistance = 4000
+        self.missileBay = 1
+
+        taskMgr.add(self.CheckIntervals, 'checkMissiles', 34)
+
+    def Thrust(self, keyDown):
+        if keyDown:
+            taskMgr.add(self.ApplyThrust, 'forward-thrust')
+        else:
+            taskMgr.remove('forward-thrust')
+    
+    def ApplyThrust(self, task):
+        rate = 5
+        trajectory = render.getRelativeVector(self.modelNode, Vec3.forward())
+        trajectory.normalize()
+        self.modelNode.setFluidPos(self.modelNode.getPos() + trajectory * rate)
+
+        return Task.cont
+    
+    def LeftTurn(self, keyDown):
+        if keyDown:
+            taskMgr.add(self.ApplyLeftTurn, 'left-turn')
+        else:
+            taskMgr.remove('left-turn')
+
+    def ApplyLeftTurn(self, task):
+        rate = .5
+        self.modelNode.setH(self.modelNode, rate)
+
+        return Task.cont
+    
+    def RightTurn(self, keyDown):
+        if keyDown:
+            taskMgr.add(self.ApplyRightTurn, 'right-turn')
+        else:
+            taskMgr.remove('right-turn')
+
+    def ApplyRightTurn(self, task):
+        rate = -.5
+        self.modelNode.setH(self.modelNode, rate)
+
+        return Task.cont
+    
+    def UpTurn(self, keyDown):
+        if keyDown:
+            taskMgr.add(self.ApplyUpTurn, 'up-turn')
+        else:
+            taskMgr.remove('up-turn')
+
+    def ApplyUpTurn(self, task):
+        rate = .5
+        self.modelNode.setP(self.modelNode, rate)
+
+        return Task.cont
+    
+    def DownTurn(self, keyDown):
+        if keyDown:
+            taskMgr.add(self.ApplyDownTurn, 'down-turn')
+        else:
+            taskMgr.remove('down-turn')
+
+    def ApplyDownTurn(self, task):
+        rate = -.5
+        self.modelNode.setP(self.modelNode, rate)
+
+        return Task.cont
+    
+    def RotateRight(self, keyDown):
+        if keyDown:
+            taskMgr.add(self.ApplyRotateRight, 'rotate-right')
+        else:
+            taskMgr.remove('rotate-right')
+
+    def ApplyRotateRight(self, task):
+        rate = .5
+        self.modelNode.setR(self.modelNode, rate)
+
+        return Task.cont
+    
+    def RotateLeft(self, keyDown):
+        if keyDown:
+            taskMgr.add(self.ApplyRotateLeft, 'rotate-left')
+        else:
+            taskMgr.remove('rotate-left')
+
+    def ApplyRotateLeft(self, task):
+        rate = -.5
+        self.modelNode.setR(self.modelNode, rate)
+
+        return Task.cont
+    
+    def Fire(self):
+        if self.missileBay:
+            travRate = self.missileDistance
+            aim = render.getRelativeVector(self.modelNode, Vec3.forward())
+            aim.normalize()
+            fireSolution = aim * travRate
+            inFront = aim * 150
+            travVec = fireSolution + self.modelNode.getPos()
+            tag = 'Missile' + str(Missile.missileCount + 1)
+            posVec = self.modelNode.getPos() + inFront + (6.5, 0, 4)
+            currentMissile = Missile(loader, 'Assets/Phaser/phaser.egg', render, tag, posVec, 4.0)
+
+            Missile.intervals[tag] = currentMissile.modelNode.posInterval(2.0, travVec, startPos = posVec, fluid = 1)
+            Missile.intervals[tag].start()
+
+            self.missileBay -= 1
+        else:
+            if not taskMgr.hasTaskNamed('reload'):
+                print('Initializing reload...')
+                taskMgr.doMethodLater(0, self.Reload, 'reload')
+                return Task.cont
+            
+    def CheckIntervals(self, task):
+        for i in Missile.intervals:
+            if not Missile.intervals[i].isPlaying():
+                Missile.cNodes[i].detachNode()
+                Missile.fireModels[i].detachNode()
+
+                del Missile.intervals[i]
+                del Missile.fireModels[i]
+                del Missile.cNodes[i]
+                del Missile.collisionSolids[i]
+
+                print(i + ' has reached the end of its path.')
+
+                break
+        return Task.cont
+            
+    def Reload(self, task):
+        if task.time > self.reloadTime:
+            self.missileBay += 1
+            if self.missileBay > 1:
+                self.missileBay = 1
+            print("Reload complete.")
+            return Task.done
+        elif task.time <= self.reloadTime:
+            print("Reload proceeding...")
+            return Task.cont
+
+    
+    def SetKeyBindings(self):
+        self.accept('space', self.Thrust, [1])
+        self.accept('space-up', self.Thrust, [0])
+        self.accept('arrow_left', self.LeftTurn, [1])
+        self.accept('arrow_left-up', self.LeftTurn, [0])
+        self.accept('arrow_right', self.RightTurn, [1])
+        self.accept('arrow_right-up', self.RightTurn, [0])
+        self.accept('arrow_up', self.UpTurn, [1])
+        self.accept('arrow_up-up', self.UpTurn, [0])
+        self.accept('arrow_down', self.DownTurn, [1])
+        self.accept('arrow_down-up', self.DownTurn, [0])
+        self.accept('x', self.RotateRight, [1])
+        self.accept('x-up', self.RotateRight, [0])
+        self.accept('z', self.RotateLeft, [1])
+        self.accept('z-up', self.RotateLeft, [0])
+        self.accept('f', self.Fire)
+
+class Missile(SphereCollidableObject):
+    fireModels = {}
+    cNodes = {}
+    collisionSolids = {}
+    intervals = {}
+    missileCount = 0
+
+    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, posVec: Vec3, scaleVec: float = 1.0):
+        super(Missile, self).__init__(loader, modelPath, parentNode, nodeName, Vec3(0, 0, 0), 3.0)
+        self.modelNode.setScale(scaleVec)
+        self.modelNode.setPos(posVec)
+        Missile.missileCount += 1
+
+        Missile.fireModels[nodeName] = self.modelNode
+        Missile.cNodes[nodeName] = self.collisionNode
+
+        Missile.collisionSolids[nodeName] = self.collisionNode.node().getSolid(0)
+        Missile.cNodes[nodeName].show()
+
+        print("Firing torpedo #" + str(Missile.missileCount))
+
+
+class Drone(SphereCollidableObject):
+    droneCount = 0
+
+    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, posVec: Vec3, scaleVec: float):
+        super(Drone, self).__init__(loader, modelPath, parentNode, nodeName, Vec3(0, 0, 0), 3)
+        self.modelNode.setPos(posVec)
+        self.modelNode.setScale(scaleVec)
